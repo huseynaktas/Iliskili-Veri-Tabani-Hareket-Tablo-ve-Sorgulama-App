99bcb77351014d95ef07184f0d4181fe61baf90a
huseynaktas <h.akts1007@gmail.com>
2024-06-22 02:06:07 +0300

    Proje dosyası ekle.

---
+++ Iliskili Veri Tabani Hareket Tablo ve Sorgulama App/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Iliskili_Veri_Tabani_Hareket_Tablo_ve_Sorgulama_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-3JI920O\SQLEXPRESS;Initial Catalog=DbHareketTabloVeSorgulama;Integrated Security=True; ");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute Proje6",conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
